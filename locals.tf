2f8c71a716b0244306860c4235e25b95587d528f
Lewis Marsden-Lambert <lewis.lambert@zserve.co.uk>
2024-04-03 11:47:11 +0100

    added resource access manager sharing

--- locals.tf
+++ locals.tf
@@ -12,15 +12,33 @@ locals {
   config_l0 = {
     for k, v in var.ipv4_root_pools :
     k => merge(v, {
+      // Set the name to the map key
+      name = k
       // Use key as description if none explicitly set
       description = coalesce(v.description, k)
     })
   }
 
+  ram_l0 = merge([
+    for k, v in local.config_l0 : {
+      for p in v.ram_share_principals :
+      md5(join("-", [k, p])) => {
+        share     = k
+        principal = p
+      }
+    }
+  ]...)
+
   // Regional level normalised pool
   config_l1 = {
     for k, v in var.ipv4_regional_pools :
     k => merge(v, {
+      // Join the name with the parent name
+      name = join("-", [
+        local.config_l0[v.parent].name,
+        k
+      ])
+
       // Use key as description if none explicitly set
       description = join("/", [
         local.config_l0[v.parent].description,
@@ -32,10 +50,26 @@ locals {
     })
   }
 
+  ram_l1 = merge([
+    for k, v in local.config_l1 : {
+      for p in v.ram_share_principals :
+      md5(join("-", [k, p])) => {
+        share     = k
+        principal = p
+      }
+    }
+  ]...)
+
   // organisational until level normalised pool
   config_l2 = {
     for k, v in var.ipv4_ou_pools :
     k => merge(v, {
+      // Join the name with the parent name
+      name = join("-", [
+        local.config_l1[v.parent].name,
+        k
+      ])
+
       // Use key as description if none explicitly set
       description = join("/", [
         local.config_l1[v.parent].description,
@@ -46,4 +80,14 @@ locals {
       locale = coalesce(local.config_l1[v.parent].locale, v.locale, "")
     })
   }
+
+  ram_l2 = merge([
+    for k, v in local.config_l2 : {
+      for p in v.ram_share_principals :
+      md5(join("-", [k, p])) => {
+        share     = k
+        principal = p
+      }
+    }
+  ]...)
 }
--- resource_access_manager.tf
+++ resource_access_manager.tf
@@ -1,8 +1,10 @@
-/*
 resource "aws_ram_resource_share" "ipam_root" {
-  for_each = local.config_l0
+  for_each = {
+    for k, v in local.config_l0 :
+    k => v if length(v.ram_share_principals) > 0
+  }
 
-  name                      = format("IPAM %s", replace(each.value.description, "/", "-"))
+  name                      = format("ipam-%s", lower(each.value.name))
   allow_external_principals = false
 
   tags = merge(var.tags, {
@@ -10,10 +12,30 @@ resource "aws_ram_resource_share" "ipam_root" {
   })
 }
 
-resource "aws_ram_resource_share" "ipam_regional" {
-  for_each = local.config_l1
+resource "aws_ram_resource_association" "ipam_root" {
+  for_each = {
+    for k, v in local.config_l0 :
+    k => v if length(v.ram_share_principals) > 0
+  }
 
-  name                      = format("IPAM %s", replace(each.value.description, "/", "-"))
+  resource_share_arn = aws_ram_resource_share.ipam_root[each.key].arn
+  resource_arn       = aws_vpc_ipam_pool.ipv4_root[each.key].arn
+}
+
+resource "aws_ram_principal_association" "ipam_root" {
+  for_each = local.ram_l0
+
+  principal          = each.value.principal
+  resource_share_arn = aws_ram_resource_share.ipam_root[each.value.share].arn
+}
+
+resource "aws_ram_resource_share" "ipam_regional" {
+  for_each = {
+    for k, v in local.config_l1 :
+    k => v if length(v.ram_share_principals) > 0
+  }
+
+  name                      = format("ipam-%s", lower(each.value.name))
   allow_external_principals = false
 
   tags = merge(var.tags, {
@@ -21,21 +43,50 @@ resource "aws_ram_resource_share" "ipam_regional" {
   })
 }
 
-resource "aws_ram_resource_share" "ipam_ou" {
-  for_each = local.config_l2
+resource "aws_ram_resource_association" "ipam_regional" {
+  for_each = {
+    for k, v in local.config_l1 :
+    k => v if length(v.ram_share_principals) > 0
+  }
 
-  name                      = format("IPAM %s", replace(each.value.description, "/", "-"))
+  resource_share_arn = aws_ram_resource_share.ipam_regional[each.key].arn
+  resource_arn       = aws_vpc_ipam_pool.ipv4_regional[each.key].arn
+}
+
+resource "aws_ram_principal_association" "ipam_regional" {
+  for_each = local.ram_l1
+
+  principal          = each.value.principal
+  resource_share_arn = aws_ram_resource_share.ipam_regional[each.value.share].arn
+}
+
+resource "aws_ram_resource_share" "ipam_ou" {
+  for_each = {
+    for k, v in local.config_l2 :
+    k => v if length(v.ram_share_principals) > 0
+  }
+
+  name                      = format("ipam-%s", lower(each.value.name))
   allow_external_principals = false
 
   tags = merge(var.tags, {
     Name = format("IPAM OU %s", each.key)
   })
 }
-*/
 
-# resource "aws_ram_principal_association" "associations" {
-#   for_each = var.ram_principals
+resource "aws_ram_resource_association" "ipam_ou" {
+  for_each = {
+    for k, v in local.config_l2 :
+    k => v if length(v.ram_share_principals) > 0
+  }
 
-#   principal          = each.value
-#   resource_share_arn = module.tgw_eu_west_2.ram_resource_share_id
-# }
+  resource_share_arn = aws_ram_resource_share.ipam_ou[each.key].arn
+  resource_arn       = aws_vpc_ipam_pool.ipv4_ou[each.key].arn
+}
+
+resource "aws_ram_principal_association" "ipam_ou" {
+  for_each = local.ram_l2
+
+  principal          = each.value.principal
+  resource_share_arn = aws_ram_resource_share.ipam_ou[each.value.share].arn
+}

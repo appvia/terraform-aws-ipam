63447db7a640250af3531bb0003685a398d6cca6
Rohith Jayawardene <gambol99@gmail.com>
2024-05-10 18:33:46 +0100

    chore: adjusting the outputs

--- outputs.tf
+++ outputs.tf
@@ -1,9 +1,11 @@
+
 output "arn" {
-  value       = aws_vpc_ipam.this.arn
   description = "ARN of the VPC IPAM"
+  value       = aws_vpc_ipam.this.arn
 }
 
 output "root_pools" {
+  description = "Map of root IPAM pools"
   value = {
     ipv4 = {
       for k, v in aws_vpc_ipam_pool.ipv4_root :
@@ -12,22 +14,20 @@ output "root_pools" {
       }
     }
   }
-
-  description = "Map of root IPAM pools"
 }
 
 output "root_shares" {
+  description = "Map of RAM shares for root IPAM pools"
   value = {
     for k, v in aws_ram_resource_share.ipam_root :
     k => {
       arn = v.arn
     }
   }
-
-  description = "Map of RAM shares for root IPAM pools"
 }
 
 output "regional_pools" {
+  description = "Map of regional IPAM pools"
   value = {
     ipv4 = {
       for k, v in aws_vpc_ipam_pool.ipv4_regional :
@@ -36,22 +36,20 @@ output "regional_pools" {
       }
     }
   }
-
-  description = "Map of regional IPAM pools"
 }
 
 output "regional_shares" {
+  description = "Map of RAM shares for regional IPAM pools"
   value = {
     for k, v in aws_ram_resource_share.ipam_regional :
     k => {
       arn = v.arn
     }
   }
-
-  description = "Map of RAM shares for regional IPAM pools"
 }
 
 output "ou_pools" {
+  description = "Map of OU IPAM pools"
   value = {
     ipv4 = {
       for k, v in aws_vpc_ipam_pool.ipv4_ou :
@@ -60,17 +58,14 @@ output "ou_pools" {
       }
     }
   }
-
-  description = "Map of OU IPAM pools"
 }
 
 output "ou_shares" {
+  description = "Map of RAM shares for OU IPAM pools"
   value = {
     for k, v in aws_ram_resource_share.ipam_ou :
     k => {
       arn = v.arn
     }
   }
-
-  description = "Map of RAM shares for OU IPAM pools"
 }
